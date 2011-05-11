5509a5afd83839c7eb44a4950a2dca69a4e3c5b5
xoomdev <xoomdevnet@gmail.com>
2011-05-10 19:55:31 -0700

    Fix invalid reference to full_base.mk breaking build.

--- supersonic.mk
+++ supersonic.mk
@@ -113,7 +113,7 @@ $(call inherit-product, device/htc/supersonic/media_a1026.mk)
 # stuff common to all HTC phones
 $(call inherit-product, device/htc/common/common.mk)
 
-$(call inherit-product, build/target/product/full_base.mk)
+$(call inherit-product, build/target/product/full.mk)
 
 
 PRODUCT_NAME := htc_supersonic
